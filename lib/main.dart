c07aea0a0c48e9f8e23ae1b5e9b18234853da976
Khalid Alhazmi <48598891+ipkalid@users.noreply.github.com>
2021-05-17 16:22:31 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:footsteps/screens/0_auth_screens/intro_screen.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(App());
 }
 
 class MainApp extends StatelessWidget {
@@ -14,6 +15,40 @@ class MainApp extends StatelessWidget {
       initialRoute: IntroScreen.routeName,
       routes: {
         IntroScreen.routeName: (context) => IntroScreen(),
+      },
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Container();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MainApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Container();
       },
     );
   }
